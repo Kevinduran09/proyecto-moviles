3d323d18369f7e8ef967daf963c023de06fbc6a1
Kevin Andrey Durán Martínez <kevinduran.net.123@gmail.com>
2025-06-12 18:21:32 -0600

    create: ui component

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,6 +1,7 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 import Stories from '../components/Stories';
+import AvatarComponent, { Piece, AvatarStyle, OptionContext } from 'avataaars';
 
 const storiesData = [
   { id: '1', username: 'Tu historia', imageUrl: 'https://api.a0.dev/assets/image?text=You&aspect=1:1' },
@@ -21,9 +22,34 @@ const MapScreen: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
       
-       <div>
+       {/* <div>
           <Stories stories={storiesData}/>
-       </div>
+       </div> */}
+        <div>
+          Your avatar:
+          <AvatarComponent avatarStyle={AvatarStyle.Circle}          
+            topType="LongHairDreads"
+            accessoriesType="Wayfarers"
+            hairColor="Auburn"
+            facialHairType="BeardMediu"
+            clotheType="Hoodie"
+            eyeType="EyeRoll"
+            eyebrowType="FlatNatural"
+            mouthType="Smile"
+            skinColor="Tanned"
+          />
+        </div>
+        <div>
+          <Piece avatarStyle="Circle" pieceType="mouth" pieceSize="100" mouthType="Eating" />
+          <Piece avatarStyle="Circle" pieceType="eyes" pieceSize="100" eyeType="Dizzy" />
+          <Piece avatarStyle="Circle" pieceType="eyebrows" pieceSize="100" eyebrowType="RaisedExcited" />
+          <Piece avatarStyle="Circle" pieceType="accessories" pieceSize="100" accessoriesType="Round" />
+          <Piece avatarStyle="Circle" pieceType="top" pieceSize="100" topType="LongHairFro" hairColor="Red" />
+          <Piece avatarStyle="Circle" pieceType="facialHair" pieceSize="100" facialHairType="BeardMajestic" />
+          <Piece avatarStyle="Circle" pieceType="clothe" pieceSize="100" clotheType="Hoodie" clotheColor="Red" />
+          <Piece avatarStyle="Circle" pieceType="graphics" pieceSize="100" graphicType="Skull" />
+          <Piece avatarStyle="Circle" pieceType="skin" pieceSize="100" skinColor="Brown" />
+        </div>
       </IonContent>
     </IonPage>
   );
